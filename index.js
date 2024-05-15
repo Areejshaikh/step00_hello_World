292bf588e791007d08b3ec5794a21379eefd977e
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-05-15 22:32:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello World!");
+let message = "Hello World!";
+// console.log("message"); // This is not the correct way to handle a message variable.
+message = "GoodBy World";
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+let message : string = "Hello World!"
+
+// console.log("message"); // This is not the correct way to handle a message variable.
+
+// message = "GoodBy World"; //Now this time print Good By World 
+
+console.log(message);
+
